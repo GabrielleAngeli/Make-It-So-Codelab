9d05160b86b145322585eb9791748e413a635538
Gabrielle <gabrielleangelipoli@gmail.com>
2023-03-10 20:58:06 -0300

    Adding firebase dependencies

---
+++ app/src/main/java/com/example/makeitso/MakeItSoApp.kt
@@ -0,0 +1,14 @@
+package com.example.makeitso
+
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import com.example.makeitso.ui.theme.MakeitsoTheme
+
+@Composable
+fun MakeItSoApp() {
+    MakeitsoTheme {
+        Surface(color = MaterialTheme.colors.background) {
+
+        }
+    }
+}
